eec651a8f19a6c558b1b9f7916c890877e9ceb3b
JurisicMilenko <dummymailacc666@gmail.com>
2026-01-06 04:32:34 +0100

    git goof

---
+++ ISAFront/isa/app/page.tsx
@@ -0,0 +1,11 @@
+import SignupPage from "./registration"
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center p-4">
+      <main className="w-full max-w-3xl bg-white/80 backdrop-blur rounded-xl shadow-lg p-8">
+        <h1>HI</h1>
+        <SignupPage></SignupPage>
+      </main>
+    </div>
+  );
+}
---
+++ ISAFront/isa/app/registration.tsx
@@ -0,0 +1,63 @@
+"use client";
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function SignupPage() {
+    const [username, setUsername] = useState('');
+    const [name, setName] = useState('');
+    const [surname, setSurname] = useState('');
+    const [address, setAddress] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+
+    const signup = async () => {
+        try {
+            // Check for empty fields
+            if (!surname || !name || !email || !password || !confirmPassword || !address || !username) {
+                alert('Please fill in all fields.');
+                return;
+            }
+
+            if (password !== confirmPassword) {
+                alert("Passwords do not match");
+            }
+
+            const response = await axios.post('http://localhost:8080/auth/signup', {
+                name,
+                surname,
+                email,
+                password,
+                address,
+                username
+            },{ headers: { 'Accept': 'application/json',
+      'Content-Type': 'application/json' } });
+            console.log(response.data);
+        } catch (error) {
+            // Handle signup error
+            alert(error)
+        }
+    };
+
+    return (
+        <div className='text-green-500'>
+            <p>name</p>
+            <input type='text' value={name} onChange={(e) => setName(e.target.value)}></input>
+            <p>surname</p>
+            <input type='text' value={surname} onChange={(e) => setSurname(e.target.value)}></input>
+            <p>username</p>
+            <input type='text' value={username} onChange={(e) => setUsername(e.target.value)}></input>
+            <p>address</p>
+            <input type='text' value={address} onChange={(e) => setAddress(e.target.value)}></input>
+            <p>email</p>
+            <input type='text' value={email} onChange={(e) => setEmail(e.target.value)}></input>
+            <p>password</p>
+            <input type='password' value={password} onChange={(e) => setPassword(e.target.value)}></input>
+            <p>confirm password</p>
+            <input type='password' value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}></input>
+            <button onClick={signup}>sign up</button>
+        </div>
+    );
+}
+
+export default SignupPage;
