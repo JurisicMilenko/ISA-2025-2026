036ce0f58ac20e55c9f2632c44080d424a4ef366
JurisicMilenko <dummymailacc666@gmail.com>
2026-01-08 22:42:01 +0100

    login al kao

--- ISAFront/isa/app/page.tsx
+++ ISAFront/isa/app/page.tsx
@@ -1,10 +1,12 @@
+import LoginPage from "./login";
 import SignupPage from "./registration"
 export default function Home() {
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center p-4">
       <main className="w-full max-w-3xl bg-white/80 backdrop-blur rounded-xl shadow-lg p-8">
         <h1>HI</h1>
-        <SignupPage></SignupPage>
+        <SignupPage/>
+        <LoginPage/>
       </main>
     </div>
   );
